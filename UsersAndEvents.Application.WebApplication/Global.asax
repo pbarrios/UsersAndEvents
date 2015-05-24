367fbe36dad660136a16a3c33e7847b9b3730fc7
pbarrios <pbarrios@makingsense.com>
2015-05-24 17:31:42 -0300

    First commit

---
+++ UsersAndEvents.Application.WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsersAndEvents.Application.WebApplication.MvcApplication" Language="C#" %>
---
+++ UsersAndEvents.Service.Api.Billing/IBillingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersAndEvents.Service.Api.Billing
+{
+    public interface IBillingService
+    {
+    }
+}
